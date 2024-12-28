789ed873f457aaa3263f74fbb999cc52454cc7a2
Isla Birnie <islabirnie@gmail.com>
2024-12-28 12:14:43 -0600

    Tenacatita post

---
+++ _posts/2024-12-28-Tenecatita.md.m
@@ -0,0 +1,27 @@
+---
+layout: post
+title:  "My Favorite Anchorage"
+author: isla
+categories: [ autobiographical ]
+image: assets/images/16.jpg
+---
+
+Hello! How are you doing on this lovely winter day? I, for one, am having a great season. We have friends in the anchorage and all is well. Except… it hasn’t always been that way. 
+
+For the past two years we have gone South fairly fast. Usually we’re in Manzanillo by early November. But this year… that didn’t happen. It took us way longer this year though. By late December, we still hadn’t arrived at my favorite anchorage. Tenacatita! 
+
+Tenacatita has an amazing, welcoming cruiser community that I haven’t found in any other place. It’s fairly remote, the closest town being Barra de Navidad,  about three hours away (by boat). Tenacatita even has an unofficial cruiser mayor.
+
+Every Christmas the cruisers all get together on the beach at the little palapa and we do a White Elephant Gift Exchange. Have you ever done one of these? Everyone brings one or two wrapped gifts and plops it down on the table. Each person who brings a gift is given a little number. We call each number in order and the person with that number gets to choose their gift. Last year I got See’s Candy lollipops. I have friends who have gotten sets of pens, bottles of Nutella, disco lights, cookies, underwater cameras, money, craft supplies, etc. It’s always so exciting to choose a gift or… steal someone else’s. 
+
+On Friday’s we also do this amazing thing called the ‘cruiser’s raft up’. All of the dinghies come together and tie up to a single anchor. We all pass around little dishes and answer a special weekly question. Some of my favorite dishes people have brought in the past were: Christmas cookies, brownies, sushi, little sandwiches, pizza, salad, etc. We also have a conch orchestra in which everyone blows their shell horns at the same time.
+Tenacatita is a pretty great place and I haven’t even told you my favorite part yet.
+Are you ready to hear that favorite part? Wait for it…. Wait for it… 
+
+THE SURF BREAK. The Tenecatita surf break is absolutely perfect for beginners. It’s only waist deep the entire way out and there are always friends on the break. I’ve tried surfing in Barra de Navidad as well-which is fun, don’t get me wrong- but the community there is so different. Everyone on the Tenacatita break already has something in common. We all live on boats. That’s not an easy thing. We already have a respect for each other that leads us to cheer on everyone else, even if this is our first time ever meeting them. 
+
+I hadn’t surfed since last February and my entire body was aching to hit the waves. This is one of my favorite parts of boat life and I have missed it so much. 
+
+What are some of your favorite sports? Do you have a favorite vacation spot?
+Your boat-friend,
+Isla
